887499d8a38dac1f315c3f916d9566f47729c2d3
ZackarieP <51347817+ZackarieP@users.noreply.github.com>
2019-08-20 19:43:01 -0400

    More on arrays

--- Javascript/Arrays/arrays.js
+++ Javascript/Arrays/arrays.js
@@ -1,22 +1,21 @@
 var todos = [];
 
-window.setTimeout(function () {
+window.setTimeout(function() {
+	var input = prompt('What would you like to do?');
 
-    var input = prompt("What would you like to do?");
-
-    while (input != "quit") {
-
-
-        if (input == "list") {
-            console.log(todos);
-        } else if (input == "new") {
-            var newTodo = prompt("What do you have todo?");
-            todos.push(newTodo);
-        }
-        input = prompt("What would you like to do?");
-    }
-    console.log(todos);
-    console.log("Ok, you quit the app");
-
-
+	while (input !== 'quit') {
+		if (input === 'list') {
+			todos.forEach(function(todo) {
+				console.log(todo);
+			});
+			//console.log(todos);
+		} else if (input === 'new') {
+			var newTodo = prompt('What do you have todo?');
+			todos.push(newTodo);
+			console.log(newTodo + ' added to the list');
+		}
+		input = prompt('What would you like to do?');
+	}
+	console.log(todos);
+	console.log('Ok, you quit the app');
 }, 500);
