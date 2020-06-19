af91438f4d1eeea85b6df9e509e6adf373c8e394
ZackarieP <51347817+ZackarieP@users.noreply.github.com>
2020-06-19 07:26:10 -0400

    Some refactoring.

---
+++ Udemy/Java/Circle.java
@@ -0,0 +1,14 @@
+package Udemy.Java;
+
+/**
+ * Circle
+ */
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+} // end class
