b0bc0fe743d1472bdfa3fbbc4a7c360da6a2e530
ZackarieP <51347817+ZackarieP@users.noreply.github.com>
2020-06-24 10:25:12 -0400

    Added new pre-built styling for Todo list app
    Trying to focus on logic behind Todo list app before adding my own style.

--- Projects/TodoApp/index.js
+++ Projects/TodoApp/index.js
@@ -1,4 +1,4 @@
-function getTodo() {
-    var text = document.getElementById('inputText').value;
-    document.getElementById("listItem").innerHTML = text;
-}
+// function getTodo() {
+//     var text = document.getElementById('inputText').value;
+//     document.getElementById("listItem").innerHTML = text;
+// }
