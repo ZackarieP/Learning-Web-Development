d67f2ef2f9d56feb32252be1ec1e71f401ecd7e3
ZackarieP <51347817+ZackarieP@users.noreply.github.com>
2020-06-30 22:16:26 -0400

    Added logic for user to choose correct tile

--- Projects/ColorGameApp/colorGame.js
+++ Projects/ColorGameApp/colorGame.js
@@ -1,29 +1,56 @@
 var color = randomRGB();
+var winnerNum = randomNumBox();
 
 // random rgb color
 function randomRGB() {
-    return "rgb( " + randomNum() + ", " + randomNum() + ", " + randomNum() + ")";
+    return "rgb( " + randomNumRGB() + ", " + randomNumRGB() + ", " + randomNumRGB() + ")";
 }
-console.log(randomRGB());
+console.log(color);
+
 
 // random number between 0-255
-function randomNum() {
-    return Math.floor(Math.random() * 255);
+function randomNumRGB() {
+    return Math.ceil(Math.random() * 255);
 }
-console.log(randomNum());
+console.log(randomNumRGB());
+
+
+// random number between 0-5 for the indices in the "divs" array below to equal 6 in total
+function randomNumBox() {
+    return Math.floor(Math.random() * 5);
+}
+console.log(randomNumBox());
+
 
 var rgbHead = document.querySelector(".rgbHeader");
 var rgbDisplay = document.querySelector(".top-header");
-
-// on page load header receives randomized color for the game to start
-window.addEventListener("load", function () {
+// on page load, header receives randomized color for the game to start
+window.addEventListener("load", () => {
     rgbHead.innerHTML = color;
 });
 
-let divs = document.querySelector(".box-choice");
+/* on page load, boxes in game area receive random color and one 
+    index is chosen to be the winner
+*/
+let divs = document.querySelectorAll(".box-choice");
+let isWinner = false;
+let winningBox = divs[winnerNum];
 
-for (let i = 0; i < divs.length; i++) {
-    divs[i] = window.addEventListener("load", function () {
-
+// function for user choosing the correct color tile
+function winner() {
+    winningBox.style.backgroundColor = color;
+    winningBox.addEventListener("click", () => {
+        alert("You won!");
+        location.reload();
     });
 }
+window.addEventListener("load", () => {
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].style.backgroundColor = randomRGB();
+    }
+    winner();
+    while (!isWinner) {
+        // placeholder so the page does not run continuously while developing logic
+        break;
+    }
+});
