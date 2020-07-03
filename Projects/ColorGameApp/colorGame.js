a16d49cd233af620455edfa565141de6acf60764
ZackarieP <51347817+ZackarieP@users.noreply.github.com>
2020-07-02 22:37:13 -0400

    Refactoring JS and added new feature
    New feature is incorrect tiles that are chosen now disappear.

--- Projects/ColorGameApp/colorGame.js
+++ Projects/ColorGameApp/colorGame.js
@@ -1,6 +1,17 @@
 var color = randomRGB();
 var winnerNum = randomNumBox();
 
+var rgbHead = document.querySelector(".rgbHeader");
+var rgbDisplay = document.querySelector(".top-header");
+
+let divs = document.querySelectorAll(".box-choice");
+let winningBox = divs[winnerNum];
+let losingBox = divs[0];
+let winnerOfGame = document.querySelector("#winner");
+
+let newColor = document.querySelector(".newColors");
+
+
 // random rgb color
 function randomRGB() {
     return "rgb( " + randomNumRGB() + ", " + randomNumRGB() + ", " + randomNumRGB() + ")";
@@ -13,50 +24,55 @@ function randomNumRGB() {
     return Math.ceil(Math.random() * 255);
 }
 
+
 // random number between 0-5 for the indices in the "divs" array below to equal 6 in total
 function randomNumBox() {
     return Math.floor(Math.random() * 5);
 }
 
 
-var rgbHead = document.querySelector(".rgbHeader");
-var rgbDisplay = document.querySelector(".top-header");
 // on page load, header receives randomized color for the game to start
 window.addEventListener("load", () => {
-    rgbHead.innerHTML = color;
+    newColors();
 });
 
 
 /* on page load, boxes in game area receive random color and one 
     index is chosen to be the winner
+    function for user choosing the correct color tile
 */
-let divs = document.querySelectorAll(".box-choice");
-let isWinner = false;
-let winningBox = divs[winnerNum];
-let winnerOfGame = document.querySelector("#winner");
-
-// function for user choosing the correct color tile
 function winner() {
-    if (!isWinner) {
-
-    }
     winningBox.style.backgroundColor = color;
     winningBox.addEventListener("click", () => {
-        winnerOfGame.innerHTML = "You've won!";
+        winnerOfGame.textContent = "You've won!";
     });
 }
-window.addEventListener("load", () => {
-    newColors();
-});
 
-let newColor = document.querySelector(".newColors");
+
 // function for new colors
 function newColors() {
     for (let i = 0; i < divs.length; i++) {
         divs[i].style.backgroundColor = randomRGB();
     }
+    rgbHead.innerHTML = color;
     winner();
 }
+
+// remove incorrectly chosen tile from game board
+function removeTile() {
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].addEventListener("click", () => {
+            if (divs[i] != divs[winnerNum]) {
+                divs[i].classList.add("hidden");
+            }
+        });
+    }
+
+}
+removeTile();
+
+
+
 newColor.addEventListener("click", () => {
     location.reload();
 });
