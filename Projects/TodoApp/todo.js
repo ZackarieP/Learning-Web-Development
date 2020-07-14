55671e2660dd0746a1b2513bffc1b8c2ab2faf79
ZackarieP <51347817+ZackarieP@users.noreply.github.com>
2020-07-13 20:40:09 -0400

    Styling of todo list and working to display inputted todos

--- Projects/TodoApp/todo.js
+++ Projects/TodoApp/todo.js
@@ -3,12 +3,16 @@ let textInput = document.querySelector(".input");
 
 let list = document.querySelector(".todos");
 let entry = document.createElement("li");
+// let plus = document.querySelector("#plus-icon").addEventListener("click");
 
 textInput.addEventListener("keydown", function (e) {
     if (e.key == "Enter") {
-        newTodos.push(textInput.value);
-        entry.appendChild(document.createTextNode(textInput.value));
+        for (let i = 0; i < newTodos.length; i++) {
+            newTodos[i] = (this.value);
+            entry.appendChild(document.createTextNode(newTodos[i]));
+        }
         list.appendChild(entry);
         textInput.value = "";
+
     }
 });
